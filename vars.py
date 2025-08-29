385782ebff4a0b70ff8ca9d5f9ddc5f601130e1b
Error1234543 <ptaviyad21@gmail.com>
2025-08-29 11:08:11 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,12 +5,12 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "20619533"))
 API_HASH = environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8264975827:AAEpqb9JFnlqaOvSeZhqKAQdS2GbonCyz0s")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8240807456:AAGx4WvM9hmr5D94yq6VsKMteIgf7CyUpXU")
 
-OWNER = int(environ.get("OWNER", "7447651332"))
-CREDIT = environ.get("CREDIT", "𓍯𝙎𝙪𝙟𝙖𝙡⚝")
+OWNER = int(environ.get("OWNER", "8226637107"))
+CREDIT = environ.get("CREDIT", "SONIC")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7447651332').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8226637107').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
 AUTH_USER = os.environ.get('AUTH_USERS', '7447651332').split(',')
